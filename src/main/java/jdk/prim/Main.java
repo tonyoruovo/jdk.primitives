c19796d38e11b68e1f4fd0802dac151528e0d545
mathaid <tonyoruovo@gmail.com>
2024-06-28 06:16:59 +0100

    Added a BiFunction test case

--- src/main/java/jdk/prim/Main.java
+++ src/main/java/jdk/prim/Main.java
@@ -1,5 +1,7 @@
 package jdk.prim;
 
+import jdk.prim.util.function.PrimitiveBiFunction;
+
 /**
  * Hello world!
  *
@@ -7,12 +9,14 @@ package jdk.prim;
 public class Main 
 {
 	
-	public String hello() {
-		return "Hello World!";
+	public int testValue() {
+        PrimitiveBiFunction.ToChar.OfShort.AndByte f = (x, y) -> java.lang.Character.highSurrogate((x << 31) | y);
+        var g = f.andThenInt(x -> String.valueOf(x).codePointAt(0));
+		return g.applyInt((short) 0xffe3, (byte) 0xf5);
 	}
 	
     public static void main( String[] args )
     {
-        System.out.println(new Main().hello());
+        System.out.println(new Main().testValue());
     }
 }
